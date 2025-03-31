da0957bcc3c7cddb833e7d13e0b7a94b80b0e111
1428-wsx-f1 <103559814+1428-wsx-f1@users.noreply.github.com>
2025-03-31 23:56:38 +1000

    Remove duplicate instance file. Move user_accounts_script.py to models.py

--- my_app_directory/config.py
+++ my_app_directory/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///inventory.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///inventory_item.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
